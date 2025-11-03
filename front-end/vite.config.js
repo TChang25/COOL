2dba023f2d422a80565ee52629459d0c612abbef
Tommy Chang <53662659+TChang25@users.noreply.github.com>
2025-11-02 22:56:34 -0500

    Update vite.config.js

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc' 
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// https://vitejs.dev/config/ 
+export default defineConfig ( { 
+  base: '/COOL/',
+  plugins : [ react ( ) ] , 
+} )
